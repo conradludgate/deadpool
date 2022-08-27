9a52a288d1b302de8584ed35a52ba7004da657ee
Conrad Ludgate <conradludgate@gmail.com>
2022-08-27 14:19:22 +0100

    tweak

--- benches/managed.rs
+++ benches/managed.rs
@@ -1,9 +1,26 @@
-use std::{fmt::Display, thread::scope};
+use std::fmt::Display;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use tokio::task::JoinHandle;
 
 //const ITERATIONS: usize = 1_048_576;
 const ITERATIONS: usize = 1 << 15;
+
+struct Manager;
+
+#[async_trait::async_trait]
+impl deadpool::Manager for Manager {
+    type Type = ();
+    type Error = ();
+    async fn create(&self) -> Result<Self::Type, Self::Error> {
+        Ok(())
+    }
+    async fn recycle(&self, t: Self::Type) -> Option<Self::Type> {
+        Some(t)
+    }
+}
+
+type Pool = deadpool::Pool<Manager>;
 
 #[derive(Copy, Clone, Debug)]
 struct Config {
@@ -20,6 +37,12 @@ impl Display for Config {
 impl Config {
     fn operations_per_worker(&self) -> usize {
         ITERATIONS / self.workers
+    }
+
+    async fn run(self, pool: Pool) {
+        for _ in 0..self.operations_per_worker() {
+            let _ = pool.get().await;
+        }
     }
 }
 
@@ -39,37 +62,17 @@ const CONFIGS: &[Config] = &[
     Config { workers: 32, pool_size: 32 },
 ];
 
-struct Manager;
-
-#[async_trait::async_trait]
-impl deadpool::Manager for Manager {
-    type Type = ();
-    type Error = ();
-    async fn create(&self) -> Result<Self::Type, Self::Error> {
-        Ok(())
-    }
-    async fn recycle(&self, t: Self::Type) -> Option<Self::Type> {
-        Some(t)
-    }
-}
-
-type Pool = deadpool::Pool<Manager>;
-
 fn bench_get(cfg: Config) {
     let pool = Pool::builder(Manager).max_size(cfg.pool_size).build();
 
-    scope(|s| {
+    std::thread::scope(|s| {
         for _ in 0..cfg.workers {
             s.spawn(|| {
                 let runtime = tokio::runtime::Builder::new_current_thread()
                     .build()
                     .unwrap();
 
-                runtime.block_on(async {
-                    for _ in 0..cfg.operations_per_worker() {
-                        let _ = pool.get().await;
-                    }
-                });
+                runtime.block_on(cfg.run(pool.clone()));
             });
         }
     });
